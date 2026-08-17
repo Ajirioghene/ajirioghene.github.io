cc82a20c72b5034e3f6c6af9a36f544f70869f32
Ajirioghene <96798957+Ajirioghene@users.noreply.github.com>
2026-08-17 19:26:02 +0100

    Upload website files

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuButton = document.getElementById("menu-button");
+const navbar = document.getElementById("navbar")
+
+menuButton.addEventListener("click", function () {
+
+navbar.classList.toggle("active");
+});
+
